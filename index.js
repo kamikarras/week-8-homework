40f84e2eb550a8c9931ce2137ff1599801d7f984
Kami Karras <kamikarras@gmail.com>
2023-11-01 15:52:05 -0700

    get mvp of shared room in A-Frame

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let express = require('express')
 let app = express()
 
+let users = []
 
-
-// app.get('/',(req,res)=>{
-//     res.send("hello")
-//     console.log("hello")
-// })
 
 app.use('/', express.static('public'))
 
@@ -24,16 +20,31 @@ io = new io.Server(server);
 io.on("connection", (socket)=>{
     console.log("we have a new client connected")
     console.log(socket.id)
+    socket.emit("id", socket.id)
+    let user = {id: socket.id, position: {}}
+    users.push(user)
+
   
     socket.on('data', data=>{
       console.log("data recieved")
       console.log(data)
-  
-      io.emit('dataAll', data)
+        users.forEach(user=>{
+            if(user.id==data.id){
+                user.position = data.position
+            }
+        })
+      io.emit('dataAll', users)
     })
+
+    // socket.on("new-sphere",data=>{
+    //     console.log("new-sphere")
+    //     users.push(data)
+    //     console.log("users "+ users)
+    // })
   
     io.on("disconnect",()=>{
-      console.log(socket.id)
+
+      console.log("disconneted " + socket.id)
     })
   })
   
--- public/app.js
+++ public/app.js
@@ -3,29 +3,71 @@ window.addEventListener('load', function () {
     let position= { x: 0, y: 1.6, z: -3 }
     var sceneEl = document.querySelector('a-scene');
 
+    let spheres = []
+    let myId=''
+    let ids=[]
 
-    let mySphere = this.document.createElement("a-entity");
-    mySphere.setAttribute('geometry', {
-        primitive: 'sphere',
-        height: 1,
-        width: 1,
-        depth: 1
-    });
-    mySphere.setAttribute('position', position);
-    sceneEl.appendChild(mySphere)
-
-    socket.on('connect', () => {
-        console.log("Connected");
+    socket.on('connect', (socket) => {
+        console.log(socket);
+       
       });
-      socket.on('dataAll',obj=>{
-        console.log(obj);
-        mySphere.setAttribute('position', {x:obj.x, y:obj.y, z:obj.z-3});
+      socket.on('id', id=>{
+        myId=id;
+        
+        console.log("welcome " + id)
+        // let mySphere = this.document.createElement("a-entity");
+        // mySphere.setAttribute('geometry', {
+        //     primitive: 'sphere',
+        //     height: 1,
+        //     width: 1,
+        //     depth: 1
+        // });
+        // mySphere.setAttribute('material', 'color', 'red')
+        // mySphere.setAttribute('position', position);
+        // mySphere.id = 'id'
+        // sceneEl.appendChild(mySphere)
+        // spheres.push({sphere:mySphere, id: id})
+       
+
+      })
+      socket.on('dataAll',users=>{
+        console.log(users);
+        // spheres[0].sphere.setAttribute('position', {x:obj.x, y:obj.y, z:obj.z-3});
+        users.forEach(user=>{
+            
+            spheres.forEach(sphere=>{
+                if(!ids.includes(sphere.id)){
+                    ids.push(sphere.id)
+                    console.log('ids '+ids)
+                }
+                if(sphere.id == user.id){
+                    sphere.setAttribute('position',  {x:user.position.x, y:user.position.y, z:user.position.z-3});
+                    
+                }
+            })
+            if(!ids.includes(user.id)){
+                let userSphere = this.document.createElement("a-entity");
+                userSphere.setAttribute('material', 'color', '#'+Math.floor(Math.random()*16777215).toString(16))
+        userSphere.setAttribute('geometry', {
+            primitive: 'sphere',
+            height: 1,
+            width: 1,
+            depth: 1
+        });
+        userSphere.setAttribute('position', user.position);
+        userSphere.id = user.id
+        sceneEl.appendChild(userSphere)
+        spheres.push(userSphere)
+            }
+            console.log(spheres)
+        })
     })
       
 
       this.document.addEventListener("keypress",()=>{
           let camera = document.querySelector("#camera")
           position = camera.getAttribute('position')
-          socket.emit('data', position)
+          let data= {position:position, id:myId}
+          socket.emit('data',data)
       })
 })
